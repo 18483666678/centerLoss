7bbf45b45ede1b74a254ec4ce4d50d56c20685fb
tensorflow1 <tensorflow1@liev.top>
2018-09-13 17:54:39 +0800

    Optimize the neural network, it can recognize the 10 classes number now.

--- centerLossMNIST.py
+++ centerLossMNIST.py
@@ -11,23 +11,36 @@ import matplotlib.pyplot as plt
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc3 = nn.Linear(50, 2)
-        self.fc2 = nn.Linear(2, 10)
+        self.conv1_1 = nn.Conv2d(1, 32, kernel_size=5, padding=2)
+        self.prelu1_1 = nn.PReLU()
+        self.conv1_2 = nn.Conv2d(32, 32, kernel_size=5, padding=2)
+        self.prelu1_2 = nn.PReLU()
+        self.conv2_1 = nn.Conv2d(32, 64, kernel_size=5, padding=2)
+        self.prelu2_1 = nn.PReLU()
+        self.conv2_2 = nn.Conv2d(64, 64, kernel_size=5, padding=2)
+        self.prelu2_2 = nn.PReLU()
+        self.conv3_1 = nn.Conv2d(64, 128, kernel_size=5, padding=2)
+        self.prelu3_1 = nn.PReLU()
+        self.conv3_2 = nn.Conv2d(128, 128, kernel_size=5, padding=2)
+        self.prelu3_2 = nn.PReLU()
+        self.preluip1 = nn.PReLU()
+        self.ip1 = nn.Linear(128 * 3 * 3, 2)
+        self.ip2 = nn.Linear(2, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        self.out = self.fc3(x)
-        x = self.fc2(self.out)
-        # print("zeng>>", x, F.log_softmax(x, dim=1))
-        return F.log_softmax(x, dim=1), self.out
+        x = self.prelu1_1(self.conv1_1(x))
+        x = self.prelu1_2(self.conv1_2(x))
+        x = F.max_pool2d(x, 2)
+        x = self.prelu2_1(self.conv2_1(x))
+        x = self.prelu2_2(self.conv2_2(x))
+        x = F.max_pool2d(x, 2)
+        x = self.prelu3_1(self.conv3_1(x))
+        x = self.prelu3_2(self.conv3_2(x))
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, 128 * 3 * 3)
+        ip1 = self.preluip1(self.ip1(x))
+        ip2 = self.ip2(ip1)
+        return F.log_softmax(ip2, dim=1), ip1
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
